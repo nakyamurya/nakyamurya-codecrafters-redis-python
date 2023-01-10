ae92076624902d6613c34473e9d9067e077132b2
nakyamurya <nakyamurya@gmail.com>
2023-01-09 23:20:03 -0500

    stage 3

--- app/main.py
+++ app/main.py
@@ -5,9 +5,18 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_connection, _ = server_socket.accept()
-    client_connection.recv(1024)
-    client_connection.send(b"+PONG\r\n")
 
+    while True:
+        try:
+            message_receive = client_connection.recv(1024)
+            message_send = b"+PONG\r\n"
+            client_connection.send(message_send)
+        except ConnectionResetError:
+            break
+        except BrokenPipeError:
+            break
+    client_connection.close()
+        
 
 if __name__ == "__main__":
     main()
